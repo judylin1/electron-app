0916fc9634bd7aa32122c0f97415e6026202c086
judy <judith.s.lin@gmail.com>
2017-03-28 21:22:23 -0500

    add ramda, refactor, and add platinum to prices

--- prices/window.js
+++ prices/window.js
@@ -1,53 +1,59 @@
-// Run this function after the page has loaded
-$(function () {
+const R = require('ramda');
 
-var stocks = [
+// Run this function after the page has loaded
+$(() => {
+
+const stocks = [
   "CL=F", // Crude oil, http://finance.yahoo.com/quote/CL=F?p=CL=F
   "GC=F", // Gold, https://finance.yahoo.com/quote/GC=F?p=GC=F
-  "SI=F"  // Silver, https://finance.yahoo.com/quote/SI=F?p=SI=F
+  "SI=F",  // Silver, https://finance.yahoo.com/quote/SI=F?p=SI=F
+  "PL=F" // Platinum, http://finance.yahoo.com/quote/PL=F?p=PL=F
 ]
 
-var fields = 'f=pl1' // Requests the current price and previous closing price
-var symbols = 's=' + stocks.join('+')
-var url = 'https://finance.yahoo.com/d/quotes.csv?' + fields + '&' + symbols
+const fields = 'f=pl1' // Requests the current price and previous closing price
+const symbols = `s=${stocks.join('+')}`
+const url = `https://finance.yahoo.com/d/quotes.csv?${fields}&${symbols}`
 
-$.ajax(url).done(function (csv) {
+$.ajax(url).done(csv => {
   // Split the output up into an array of lines
-  var lines = csv.trim().split('\n')
+  const lines = csv.trim().split('\n');
 
-  // Iterate over each line
-  for (var i = 0; i < lines.length; i++) {
+  return lines.map((line, index) => {
     //Split the line up by comma
-    var prices = lines[i].split(',')
+    const prices = lines[index].split(',');
 
     // Previous closing price of stock symbol
-    var previousPrice = parseFloat(prices[0], 10)
+    const previousPrice = parseFloat(prices[0], 10);
 
     // Current price of stock symbol
-    var currentPrice = parseFloat(prices[1], 10)
+    const currentPrice = parseFloat(prices[1], 10);
 
     // Change between closing price and current price rounded to 2 decimal points.
-    var change = Math.round((currentPrice - previousPrice) * 100) / 100
+    const change = Math.round((currentPrice - previousPrice) * 100) / 100;
 
     // Add a leading + for positive change
-    if (change >= 0) {
-      change = '+' + change
-    }
+    const checkPosChange = (Math.round((currentPrice - previousPrice) * 100) / 100) >= 0 ? `+${change}` : change;
 
     // Add prices and changes to HTML element
-    if (i == 0) { // Oil
-      $('#oil-price').text(currentPrice.toLocaleString())
-      $('#oil-change').text(change)
-    } else if (i == 1) { // Gold
-      $('#gold-price').text(currentPrice.toLocaleString())
-      $('#gold-change').text(change)
-    } else if (i == 2) { // Silver
-      $('#silver-price').text(currentPrice.toLocaleString())
-      $('#silver-change').text(change)
-    }
-  }
-}).fail(function (error) {
-  console.error(error)
-})
+    R.call(R.cond([
+      [R.equals(0), () => {
+        $('#oil-price').text(currentPrice.toLocaleString());
+        $('#oil-change').text(checkPosChange);
+      }],
+      [R.equals(1), () => {
+        $('#gold-price').text(currentPrice.toLocaleString());
+        $('#gold-change').text(checkPosChange);
+      }],
+      [R.equals(2), () => {
+        $('#silver-price').text(currentPrice.toLocaleString());
+        $('#silver-change').text(checkPosChange);
+      }],
+      [R.equals(3), () => {
+        $('#platinum-price').text(currentPrice.toLocaleString());
+        $('#platinum-change').text(checkPosChange);
+      }],
+    ]), index);
+  })
+}).fail(error => console.error(error))
 
 })
